f4079b8b42ba9d74e6c6b7eec048f87c9d935cfa
vdsouza22 <vinitadsouza22@gmail.com>
2020-06-20 15:52:27 +0530

    Added changes for test cases

--- src/test/java/com/assesment/cognizant/test/CarGarageApplicationTests.java
+++ src/test/java/com/assesment/cognizant/test/CarGarageApplicationTests.java
@@ -2,6 +2,7 @@ package com.assesment.cognizant.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,6 +11,7 @@ import com.assesment.cognizant.CarGarageApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CarGarageApplication.class)
+@AutoConfigureMockMvc
 public class CarGarageApplicationTests {
 
 	@Test
